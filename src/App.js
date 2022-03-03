1cd3e0f86300f19ae739bda30a54bc68c861046a
sneha2395 <agarwal.sneha.231995@gmail.com>
2022-03-03 18:44:24 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './components/about/About';
+import Intro from './components/intro/Intro';
+import Product from './components/product/Prouduct';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi ...this is sneha</h1>
+       <Intro/>
+     <About/>
+    
+     {/* <Contact/> */}
+     <Product/>
     </div>
   );
 }
