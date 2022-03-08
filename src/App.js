f277ea23e74c0de26253248dc7f8f0bd2ed72d8c
sneha2395 <agarwal.sneha.231995@gmail.com>
2022-03-08 16:19:24 +0530

    contact section done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
 import About from './components/about/About';
 import Intro from './components/intro/Intro';
-// import Product from './components/product/Product';
-// import Contact from './components/contact/Contact';
 import ProductList from './components/productList/ProductList';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
     <div className="App">
        <Intro/>
      <About/>
-    
-     {/* <Contact/> */}
      <ProductList/>
-
+     <Contact/>
     </div>
   );
 }
